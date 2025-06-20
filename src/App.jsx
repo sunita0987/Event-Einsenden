7978c82d88463818f9404162cd4cefc29716c6e4
sunita0987 <sunita0987546@gmail.com>
2025-06-20 10:42:16 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+
+  return (
+    <>
+      <Heading/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Heading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h1 className=" text-xl font-bold ff-open sans text-center mt-10">
+        Event einsenden
+      </h1>
+      <p className=" text-lg font-light ff-open sans text-[#737376] text-center mt-5">
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren,<br/> no sea takimata sanctus est Lorem ipsum dolor sit amet.At
+        vero eos et <br/> accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren, no sea takimata sanctus est <br/> Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  );
+}
+
+export default Heading;
